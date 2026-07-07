fdae1cbb22c1063bd2e71ee69eccc5e236bf25a5
OMAR-A-M-A <oa5252842@gmail.com>
2026-07-07 05:38:21 +0300

    set the react-toast and display delete notification

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 const TableRow = styled.div`
   display: grid;
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
@@ -49,12 +50,12 @@ function CabinRow({ cabin }) {
   const { isPending: isDeleteing, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
-      alert("Cabin deleted successfully");
+      toast.success("Cabin deleted successfully");
       qeuryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
     },
-    onError: (err) => alert(err.message),
+    onError: (err) => toast.error(err.message),
   });
   return (
     <TableRow role="row">
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -13,14 +13,22 @@ const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
 `;
-
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem    ;
+`;
 function AppLayout() {
   return (
     <StyledAppLayout>
       <Header></Header>
       <Sidebar></Sidebar>
       <Main>
-        <Outlet></Outlet>
+        <Container>
+          <Outlet></Outlet>
+        </Container>
       </Main>
     </StyledAppLayout>
   );
