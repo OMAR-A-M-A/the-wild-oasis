88d907b8d32292fac2e44c189e99ab3248b2e578
OMAR-A-M-A <oa5252842@gmail.com>
2026-07-15 08:33:53 +0300

    implement a the dark mode theme

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useEffect } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+
+const DarkMode = createContext();
+
+function DarkModeContext({ children }) {
+  const [isDark, setIsDark] = useLocalStorageState(false, "isDarkMode");
+
+  function handleToggleDarkMode() {
+    setIsDark((dark) => !dark);
+  }
+
+  useEffect(
+    function () {
+      if (isDark) {
+        document.documentElement.classList.add("dark-mode");
+        document.documentElement.classList.remove("light-mode");
+      } else {
+        document.documentElement.classList.add("light-mode");
+        document.documentElement.classList.remove("dark-mode");
+      }
+    },
+    [isDark],
+  );
+  return (
+    <DarkMode.Provider value={{ isDark, handleToggleDarkMode }}>
+      {children}
+    </DarkMode.Provider>
+  );
+}
+
+function useDarkMode() {
+  const context = useContext(DarkMode);
+  if (context === undefined)
+    throw new Error("DarkModeContext was used outside of DarkModeProvider");
+  return context;
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export { DarkModeContext, useDarkMode };
